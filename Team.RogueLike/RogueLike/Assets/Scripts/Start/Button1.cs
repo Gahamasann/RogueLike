2d29a0a0e343ca7ebc370069bca82cc13ad804af
yuki1108 <yuki1108.don.3dango@gmail.com>
2019-06-29 12:21:49 +0900

    最初の村(テスト)追加

---
+++ Team.RogueLike/RogueLike/Assets/Scripts/Start/Button1.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button1 : MonoBehaviour
+{
+    public string nextScene;
+    public void OnClick()
+    {
+        FadeSceneManager.FadeOut(nextScene);
+    }
+}
---
+++ Team.RogueLike/RogueLike/Assets/Scripts/Start/KitiPlayer.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KitiPlayer : MonoBehaviour
+{
+    public float speed;
+    Vector3 velocity;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    //移動処理
+    void Move()
+    {
+        velocity = Vector3.zero;
+        if (Input.GetAxisRaw("Horizontal") == 1)
+        {
+            if (transform.position.x <= 7.7)
+            {
+                velocity.x = 1;
+            }
+            else
+            {
+                velocity.x = 0;
+            }
+        }
+        if (Input.GetAxisRaw("Horizontal") == -1)
+        {
+            if (transform.position.x >= -7.7)
+            {
+                velocity.x = -1;
+            }
+            else
+            {
+                velocity.x = 0;
+            }
+        }
+        if (Input.GetAxisRaw("Vertical") == 1)
+        {
+            if (transform.position.y <= 9.2)
+            {
+                velocity.y = 1;
+            }
+            else
+            {
+                velocity.y = 0;
+            }
+        }
+        if (Input.GetAxisRaw("Vertical") == -1)
+        {
+            if (transform.position.y >= -9.2)
+            {
+                velocity.y = -1;
+            }
+            else
+            {
+                velocity.y = 0;
+            }
+        }
+
+        velocity.Normalize();//正規化
+        transform.position += velocity * speed * Time.deltaTime;
+    }
+}
